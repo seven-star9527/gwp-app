ba8590faa2b1e46bdc6cc91c6740653b9d808c12
Seven <rajonpontiflet@gmail.com>
2026-04-21 20:44:45 +0800

    fix: add Polaris AppProvider and styles to app layout

--- app/routes/app.jsx
+++ app/routes/app.jsx
@@ -1,12 +1,16 @@
 // app/routes/app.jsx
 import { Outlet, useLoaderData, useRouteError } from "react-router";
 import { boundary } from "@shopify/shopify-app-react-router/server";
-import { AppProvider } from "@shopify/shopify-app-react-router/react";
+import { AppProvider as AppBridgeProvider } from "@shopify/shopify-app-react-router/react";
+import { AppProvider as PolarisAppProvider } from "@shopify/polaris";
+import enTranslations from "@shopify/polaris/locales/en.json";
+import polarisStyles from "@shopify/polaris/build/esm/styles.css?url";
 import { authenticate } from "../shopify.server";
+
+export const links = () => [{ rel: "stylesheet", href: polarisStyles }];
 
 export const loader = async ({ request }) => {
   await authenticate.admin(request);
-  // eslint-disable-next-line no-undef
   return { apiKey: process.env.SHOPIFY_API_KEY || "" };
 };
 
@@ -14,13 +18,15 @@ export default function App() {
   const { apiKey } = useLoaderData();
 
   return (
-    <AppProvider embedded apiKey={apiKey}>
-      <s-app-nav>
-        <s-link href="/app">Dashboard</s-link>
-        <s-link href="/app/campaign/new">New Campaign</s-link>
-      </s-app-nav>
-      <Outlet />
-    </AppProvider>
+    <AppBridgeProvider embedded apiKey={apiKey}>
+      <PolarisAppProvider i18n={enTranslations}>
+        <ui-nav-menu>
+          <a href="/app" rel="home">Dashboard</a>
+          <a href="/app/campaign/new">New Campaign</a>
+        </ui-nav-menu>
+        <Outlet />
+      </PolarisAppProvider>
+    </AppBridgeProvider>
   );
 }
 
